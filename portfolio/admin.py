6751c8e543d8f1615d29cafb2d6aa8c7ff787aa0
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-01-30 16:50:11 +0100

    fin design scree ordi

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -6,8 +6,9 @@ from portfolio.models import Categorie, Project, Certificat, Tag
 class ProjectAdmin(admin.ModelAdmin):
     class Meta:
         model = Project
-        list_display = ('title', 'slug', 'image', 'description', 'date', 'status', 'categorie', 'tag')
-        list_filter = ('categorie', 'tag')
+        list_display = ('title', 'slug', 'project_thumbnail', 'description', 'date_created_on', 'status', 'categorie',
+                        'tags')
+        list_filter = ('categorie', 'tags')
         search_fields = ('title', 'description')
         prepopulated_fields = {'slug': ('title',)}
 
